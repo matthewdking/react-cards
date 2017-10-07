a271c3a4d8138abb3d418e13231acd075d9ae3ad
matthewdking <matthew.d.king@outlook.com>
2017-10-07 20:37:14 +0300

    finishes card imports

--- src/components/card_urls.jsx
+++ src/components/card_urls.jsx
@@ -26,6 +26,34 @@ import HeartsJack from '../../public/assets/jack_of_hearts.png';
 import HeartsQueen from '../../public/assets/queen_of_hearts.png';
 import HeartsKing from '../../public/assets/king_of_hearts.png';
 
+import SpadesAce from '../../public/assets/ace_of_spades.png';
+import Spades2 from '../../public/assets/2_of_spades.png';
+import Spades3 from '../../public/assets/3_of_spades.png';
+import Spades4 from '../../public/assets/4_of_spades.png';
+import Spades5 from '../../public/assets/5_of_spades.png';
+import Spades6 from '../../public/assets/6_of_spades.png';
+import Spades7 from '../../public/assets/7_of_spades.png';
+import Spades8 from '../../public/assets/8_of_spades.png';
+import Spades9 from '../../public/assets/9_of_spades.png';
+import Spades10 from '../../public/assets/10_of_spades.png';
+import SpadesJack from '../../public/assets/jack_of_spades.png';
+import SpadesQueen from '../../public/assets/queen_of_spades.png';
+import SpadesKing from '../../public/assets/king_of_spades.png';
+
+import DiamondsAce from '../../public/assets/ace_of_diamonds.png';
+import Diamonds2 from '../../public/assets/2_of_diamonds.png';
+import Diamonds3 from '../../public/assets/3_of_diamonds.png';
+import Diamonds4 from '../../public/assets/4_of_diamonds.png';
+import Diamonds5 from '../../public/assets/5_of_diamonds.png';
+import Diamonds6 from '../../public/assets/6_of_diamonds.png';
+import Diamonds7 from '../../public/assets/7_of_diamonds.png';
+import Diamonds8 from '../../public/assets/8_of_diamonds.png';
+import Diamonds9 from '../../public/assets/9_of_diamonds.png';
+import Diamonds10 from '../../public/assets/10_of_diamonds.png';
+import DiamondsJack from '../../public/assets/jack_of_diamonds.png';
+import DiamondsQueen from '../../public/assets/queen_of_diamonds.png';
+import DiamondsKing from '../../public/assets/king_of_diamonds.png';
+
 const Cards = [
   ClubsAce,
   Clubs2,
@@ -52,7 +80,33 @@ const Cards = [
   Hearts10,
   HeartsJack,
   HeartsQueen,
-  HeartsKing
+  HeartsKing,
+  SpadesAce,
+  Spades2,
+  Spades3,
+  Spades4,
+  Spades5,
+  Spades6,
+  Spades7,
+  Spades8,
+  Spades9,
+  Spades10,
+  SpadesJack,
+  SpadesQueen,
+  SpadesKing,
+  DiamondsAce,
+  Diamonds2,
+  Diamonds3,
+  Diamonds4,
+  Diamonds5,
+  Diamonds6,
+  Diamonds7,
+  Diamonds8,
+  Diamonds9,
+  Diamonds10,
+  DiamondsJack,
+  DiamondsQueen,
+  DiamondsKing
 ];
 
 export default Cards;
